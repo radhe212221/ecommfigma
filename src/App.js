00878bc13923bb6063fe46b3dead2fca68a5c6f0
RS <radhe212221@gmail.com>
2022-05-28 17:31:36 +0530

    added page skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProductsPage from './pages/ProductsPage'
+import DetailsPage from './pages/DetailsPage'
+import CategoryPage from './pages/CategoryPage'
 function App() {
   return (
-    <div>
-      Ecommerce Figma
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/new" component={ProductsPage} />
+        <Route exact path="/details" component={DetailsPage} />
+        <Route exact path="/Catgeory" component={CategoryPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
